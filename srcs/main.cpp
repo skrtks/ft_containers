b520afcc578620e6dba6f3ef2390ee9a422a0462
Sam Kortekaas <samkortekaas@protonmail.com>
2020-10-18 19:43:43 +0200

    Small test with meta function

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -15,14 +15,56 @@
 #include "list.hpp"
 #include <list>
 
-int main() {
-	std::list<int> myList;
-	std::list<std::string> stringList;
-	myList.push_back(1);
-	myList.push_back(2);
-	myList.push_back(3);
+template <class T>
+struct is_pointer
+{
+	template <class U>
+	static char is_ptr(U *);
 
-	stringList.assign(myList.begin(), myList.end());
+	template <class X, class Y>
+	static char is_ptr(Y X::*);
 
-	return 0;
+	template <class U>
+	static char is_ptr(U (*)());
+
+	static double is_ptr(...);
+
+	static T t;
+	enum { value = sizeof(is_ptr(t)) == sizeof(char) };
+};
+
+struct Foo {
+	int bar;
+};
+
+int main(void)
+{
+//	typedef int * IntPtr;
+	typedef int Foo::* FooMemberPtr;
+	typedef int (*FuncPtr)();
+
+	printf("%d\n",is_pointer<int>::value);        // prints 1
+	printf("%d\n",is_pointer<FooMemberPtr>::value);  // prints 1
+	printf("%d\n",is_pointer<FuncPtr>::value);       // prints 1
 }
+
+//int main() {
+//	ft::list<int> myList;
+//	ft::list<int> stringList;
+//	myList.push_back(1);
+//	myList.push_back(2);
+//	myList.push_back(3);
+//
+//	stringList.assign(myList.begin(), myList.end());
+//	for (ft::list<int>::iterator it = stringList.begin(); it != stringList.end(); it++) {
+//		std::cout << " " << *it;
+//	}
+//	std::cout << std::endl;
+//
+////	stringList.assign(4, 40);
+////	for (ft::list<int>::iterator it = stringList.begin(); it != stringList.end(); it++) {
+////		std::cout << " " << *it;
+////	}
+////	std::cout << std::endl;
+//	return 0;
+//}
