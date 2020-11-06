c4701b2069266b32f2059856d2ea9137c72e61f9
Sam Kortekaas <samkortekaas@protonmail.com>
2020-11-06 18:17:08 +0100

    Removed relational overloads for now

--- srcs/map/mapNode.hpp
+++ srcs/map/mapNode.hpp
@@ -93,26 +93,6 @@ namespace ft {
 			}
 			return curr;
 		}
-
-		bool	operator==(const mapNode& other) {
-			return (this->_data == other._data);
-		}
-		bool	operator!=(const mapNode& other) {
-			return (this->_data != other._data);
-		}
-		bool	operator>(const mapNode& other) {
-			return (this->_data > other._data);
-		}
-		bool	operator<(const mapNode& other) {
-			return (this->_data < other._data);
-		}
-		bool 	operator<=(const mapNode& other) {
-			return (this->_data <= other._data);
-		}
-		bool 	operator>=(const mapNode& other) {
-			return (this->_data >= other._data);
-		}
-
 		void setParent(mapNode* parent) {
 			_parent = parent;
 		}
